fd0c3fd48d0688cc5f894dedce3d312ae30be951
ahmd199106 <ahmd199106@gmail.com>
2022-12-31 20:46:48 +0000

    first commit - add storybook and mantine ui to t3 stack

---
+++ .storybook/preview.js
@@ -0,0 +1,36 @@
+import { MantineProvider } from '@mantine/core';
+import React from 'react';
+import { theme } from './theme';
+
+// Create a wrapper component that will contain all your providers.
+// Usually you should render all providers in this component:
+// MantineProvider, NotificationsProvider, ModalsProvider, SpotlightProvider, etc.
+// export const MantineProviderWrapper = ({ children }) => {
+//   return (
+//     <MantineProvider theme={theme}>
+//       {children}
+//     </MantineProvider>
+//   );
+// };
+function ThemeWrapper(props) {
+  return (
+    <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+      {props.children}
+    </MantineProvider>
+  );
+}
+
+// enhance your stories with decorator that uses ThemeWrapper
+export const decorators = [
+  (renderStory) => <ThemeWrapper>{renderStory()}</ThemeWrapper>,
+];
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
---
+++ src/pages/_app.tsx
@@ -0,0 +1,32 @@
+import { type AppType } from 'next/app';
+import { type Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
+import { MantineProvider } from '@mantine/core';
+import {theme} from '../../.storybook/theme'
+
+import { trpc } from '../utils/trpc';
+
+import '../styles/globals.css';
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={theme}
+        // theme={{
+        //   /** Put your mantine theme override here */
+        //   colorScheme: 'light',
+        // }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </SessionProvider>
+  );
+};
+
+export default trpc.withTRPC(MyApp);
